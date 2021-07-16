16228896156c57764ce292a57ddb3e1d0e60d8f6
LuisP <luisjavierpolancomorel@gmail.com>
2021-07-16 04:51:07 -0400

    Updates

---
+++ precache-manifest.85a5d1de5fe68a8d6c989bb82619a827.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45faa22e9d9d30f12cbeb38afb87c2f9",
+    "url": "/checkers-app/index.html"
+  },
+  {
+    "revision": "c30238b2d695c013d48d",
+    "url": "/checkers-app/static/css/main.18fa541f.chunk.css"
+  },
+  {
+    "revision": "9a631e22492c4da05452",
+    "url": "/checkers-app/static/js/2.7d431426.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/checkers-app/static/js/2.7d431426.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c30238b2d695c013d48d",
+    "url": "/checkers-app/static/js/main.6c479ab7.chunk.js"
+  },
+  {
+    "revision": "2739d430df7c046dbb71",
+    "url": "/checkers-app/static/js/runtime-main.146cdd60.js"
+  }
+]);
